d01253da5905baf742787c03ef3681ea65709247
angelapesina <56794654+angelapesina@users.noreply.github.com>
2021-02-18 12:24:28 -0600

    Create 1-3Variations1-2.c

---
+++ 1-3Variations1-2.c
@@ -0,0 +1,19 @@
+/*#include <stdio.h>
+main (){
+	int far;
+	
+	for (far = 0; far <= 300; far + 20)
+	printf("%3d %6.1f\n", far, (5.0/9.0)*(far-32));
+}
+*/
+// /*Exersice part: Reverse order.
+
+#include <stdio.h>
+main (){
+	int far;
+	
+	for (far = 300; far >= 0; far - 20)
+	printf("%3d %6.1f\n", far, (5.0/9.0)*(far-32));
+}
+
+//*/
