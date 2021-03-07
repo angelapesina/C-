187a51d65195ce5efe6a2f5ac91acb58e9d9aeef
angelapesina <56794654+angelapesina@users.noreply.github.com>
2021-03-07 12:33:48 -0600

    Create exercise17.c

---
+++ Chapter 1/book examples/exercise17.c
@@ -0,0 +1,30 @@
+//Write a program to print all input lines that are longer than 80 characters
+
+#include <stdio.h>
+
+#define MAXLEN 1000
+#define MINLEN 80
+
+int main() {
+    int c;
+    int index = 0;
+    char line[MAXLEN];
+
+    while ((c = getchar()) != EOF)  {
+        if (c != '\n')
+        {
+            line[index] = c;
+            ++index;
+        }
+        else {
+            line[index] = '\0';
+
+            if (index >= MINLEN)  {
+                printf("%s\n", line);
+            }
+            index = 0;
+        }
+    }
+
+    return 0;
+}
