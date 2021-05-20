74bdc990ff845f2cf30dc1edc0c41cf12c7bae05
angelapesina <56794654+angelapesina@users.noreply.github.com>
2021-05-20 01:57:14 -0500

    Create res.h

---
+++ 1****PROYECTO****/res.h
@@ -0,0 +1,16 @@
+
+//functions 
+void custo_order();
+void b_menu();
+void l_menu();
+void di_menu();
+void d_menu();
+void deli_menu();
+void ticket();
+void opinion();
+void em_options();
+void salary();
+void em_disco();
+
+//global var
+int bpago, lpago, dipago, dpago, delipago, pagototal;
